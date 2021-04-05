fe6d9dad4552247172066da7c614fb42a97b1dfe
Giles Thompson <giles@papaiti.com>
2021-04-05 17:05:56 +1200

    Test with bgantzler's staticaddontrees

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,19 @@
-export default function() {
+/* global requirejs */
 
-  // These comments are here to help you get started. Feel free to delete them.
+import { createServer, discoverEmberDataModels } from 'ember-cli-mirage';
 
-  /*
-    Config (with defaults).
 
-    Note: these only affect routes defined *after* them!
-  */
+export function makeServer(config) {
+  let finalConfig = {
+    ...config,
+    models: { ...discoverEmberDataModels({ moduleMap: requirejs.entries }), ...config.models },
+    routes
+  };
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  return createServer(finalConfig)
+}
 
-  /*
-    Shorthand cheatsheet:
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
+function routes() {
 
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
 }
